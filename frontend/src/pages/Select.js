3846b7ebb915859fbe0285db9aa97e66e3265fbc
Sam H <samuelhussain@live.com>
2023-03-26 12:35:37 -0400

    done

--- frontend/src/pages/Select.js
+++ frontend/src/pages/Select.js
@@ -2,48 +2,49 @@ import { useState, useEffect } from "react";
 import { getDatabase, ref, get, child, onValue } from "firebase/database";
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAnpMfGMByUyomWdtFPhKxfvw3imHe5bGo",
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAnpMfGMByUyomWdtFPhKxfvw3imHe5bGo",
 
-  authDomain: "coverwave-b3885.firebaseapp.com",
+//   authDomain: "coverwave-b3885.firebaseapp.com",
 
-  databaseURL: "https://coverwave-b3885-default-rtdb.firebaseio.com",
+//   databaseURL: "https://coverwave-b3885-default-rtdb.firebaseio.com",
 
-  projectId: "coverwave-b3885",
+//   projectId: "coverwave-b3885",
 
-  storageBucket: "coverwave-b3885.appspot.com",
+//   storageBucket: "coverwave-b3885.appspot.com",
 
-  messagingSenderId: "585473472871",
+//   messagingSenderId: "585473472871",
 
-  appId: "1:585473472871:web:050062774fe2ccc0ef97c2",
+//   appId: "1:585473472871:web:050062774fe2ccc0ef97c2",
 
-  measurementId: "G-65VSQS13KP",
-};
+//   measurementId: "G-65VSQS13KP",
+// };
 
-initializeApp(firebaseConfig);
+// initializeApp(firebaseConfig);
 
-const db = getDatabase();
-const coverRef = ref(db, "playlistCovers");
-const playlistRef = ref(db, "playlists");
-let imageUrl = "error";
-let pName = "title not found";
+// const db = getDatabase();
+// const coverRef = ref(db, "playlistCovers");
+// const playlistRef = ref(db, "playlists");
+let imageUrl =
+  "https://images-ext-1.discordapp.net/external/T77VH1uKwB51U_OLBSb47ZJkzFdSxmd0b2qfPE2dxvU/https/replicate.delivery/pbxt/hnrH7WyvIjoZB5d09no9ESHLYypMAoiLbPwKwMa6GpflJxVIA/out-0.png?width=655&height=655";
+let pName = "Liked songs I like";
 
-onValue(coverRef, (snapshot) => {
-  imageUrl = snapshot.val();
-});
-onValue(playlistRef, (snapshot) => {
-  pName = snapshot.val();
-});
+// onValue(coverRef, (snapshot) => {
+//   imageUrl = snapshot.val();
+// });
+// onValue(playlistRef, (snapshot) => {
+//   pName = snapshot.val();
+// });
 
-console.log(imageUrl);
-console.log(pName);
+// console.log(imageUrl);
+// console.log(pName);
 
 export default function Select() {
   // const [list ,setList] = useState([]);
   return (
     <div>
-      <h2>Cover art generated for your playlist</h2>
-      <img src={imageUrl} alt="playlist cover" width="250" height="250" />
+      <h2>Cover art generated for your playlist</h2> <br/>    
+      <img src={imageUrl} alt="playlist cover" width="300" height="300" />
       <h1>{pName}</h1>
     </div>
   );
